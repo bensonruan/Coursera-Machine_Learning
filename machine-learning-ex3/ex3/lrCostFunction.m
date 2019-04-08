06bc32b994a12ed7acdf4a2e476a982c33df2037
Benson Ruan <benson_ruan@hotmail.com>
2019-04-08 21:21:40 +1000

    Machine Learning ex3
    Machine Learning ex3 : Multi-class Classication and Neural Networks
    Finished and passed Assigement

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,15 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+mask = [0; ones(size(theta,1)-1, size(theta,2))]; 
 
+reg = (lambda/(2 * m)) * sum((theta .* mask) .^ 2);
 
+hy = sigmoid(X * theta);
 
+J = ((-1/m) * sum(y .* log(hy) + (1 - y) .* log(1 - hy))) + reg;
 
-
-
-
-
+grad = ((1/m) * (X' * (hy - y))) + (lambda/m) * (theta .* mask);
 
 % =============================================================
 
--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -28,11 +28,11 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
+%    
 
+p_sig = sigmoid(X * all_theta');
 
-
-
+[max_values p] =  max( p_sig, [], 2);
 
 
 
